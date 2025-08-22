bf94322955fdb8696545f1f900af2c5cd5812caa
fabiconcept <favourajokubi@gmail.com>
2025-08-22 14:52:19 +0100

    Make Updates from corrections

--- utils/constants/navigation.ts
+++ utils/constants/navigation.ts
@@ -44,7 +44,7 @@ export const NAV_LINKS: NavLink[] = [
 
 export const CONTACT_INFO = {
   address: "Block 4, Imo State Secretariat, Port Harcourt Road, Owerri, Imo State",
-  email: "info@finance.imostate.gov.ng",
+  email: "info@finance.im.gov",
   phone: "083-230940",
   hours: "Monday to Friday, 8:00 AM – 4:00 PM"
 };
